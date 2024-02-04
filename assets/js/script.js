cc3460902250715c78734642955eae725862bb41
niekados <tiny.post@yahoo.com>
2024-02-04 21:14:27 +0000

    Add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,8 +44,7 @@ function runGame(gameType) {
     displaySubtractQuestion(num1, num2);
   }
   else {
-    alert(`Unknown game type: ${gameType}`);
-    throw `Unknown game type: ${gameType}. Aborting!`
+    displayDivisionQuestion(num1,num2);
   }
 }
 
@@ -88,8 +87,7 @@ function calculateCorrectAnswer() {
   } else if (operator === "-") {
   return [operand1 - operand2, "subtract"];
   } else {
-    alert(`Unimplemented operator ${operator}`);
-    throw `Unimplemented operator ${operator}. Aborting!`;
+    return [operand1 / operand2, "division"]
   }
 
 }
@@ -128,5 +126,13 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
   document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+  document.getElementById('operand1').textContent = operand1 * operand2;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "/";
 
 }
